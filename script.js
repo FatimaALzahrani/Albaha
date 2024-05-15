9c87274934501f555c01898732d9328920ebee07
FatimaALzahrani <12fatimah.15@gmail.com>
2024-05-15 21:56:27 +0300

    .

--- script.js
+++ script.js
@@ -6,18 +6,120 @@ $(document).ready(function () {
     })
 })
 
-document.addEventListener("DOMContentLoaded", function() {
-    const animationContainer = document.querySelector(".animation-container");
 
-    function startAnimation() {
-        animationContainer.classList.add("move");
-    }
+const animatedElements = document.querySelectorAll('.anim');
 
-    function stopAnimation() {
-        animationContainer.classList.remove("move");
-    }
+        function handleIntersection(entries, observer) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show');
+                } else {
+                    entry.target.classList.remove('show');
+                }
+            });
+        }
 
-    animationContainer.addEventListener("mouseover", startAnimation);
-    animationContainer.addEventListener("mouseout", stopAnimation);
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5
+        };
+
+        const observer = new IntersectionObserver(handleIntersection, options);
+
+        animatedElements.forEach(element => {
+            observer.observe(element);
+        });
+
+        // animation 4
+        const animatedElements4 = document.querySelectorAll('.anim4');
+
+        function handleIntersection4(entries, observer4) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show4');
+                } else {
+                    entry.target.classList.remove('show4');
+                }
+            });
+        }
+
+        const options4 = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5
+        };
+
+        const observer4 = new IntersectionObserver(handleIntersection4, options4);
+
+        animatedElements4.forEach(element => {
+            observer4.observe(element);
+        });
+        // animation 5
+        const animatedElements5 = document.querySelectorAll('.anim5');
+
+        function handleIntersection5(entries, observer5) {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show4');
+                } else {
+                    entry.target.classList.remove('show4');
+                }
+            });
+        }
+
+        const options5 = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5
+        };
+
+        const observer5 = new IntersectionObserver(handleIntersection5, options5);
+
+        animatedElements5.forEach(element => {
+            observer5.observe(element);
+        });
+         // animation 3
+         const animatedElements3 = document.querySelectorAll('.anim3');
+
+         function handleIntersection3(entries, observer3) {
+             entries.forEach(entry => {
+                 if (entry.isIntersecting) {
+                     entry.target.classList.add('show3');
+                 } else {
+                     entry.target.classList.remove('show3');
+                 }
+             });
+         }
+ 
+         const options3 = {
+             root: null,
+             rootMargin: '0px',
+             threshold: 0.5
+         };
+ 
+         const observer3 = new IntersectionObserver(handleIntersection3, options3);
+ 
+         animatedElements3.forEach(element => {
+             observer3.observe(element);
+         });
+const sliderContainer = document.querySelector('.slider-container')
+const slideRight = document.querySelector('.right-slide')
+const slideLeft = document.querySelector('.left-slide')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
+
+
+
+// counter
+$('.count').each(function () {
+    $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now) + '+'); 
+        }
+    });
 });
-
